1d682553f03f4708863e81365cc2c84365592f17
Alex <sunnycodes>
2021-03-12 13:17:00 -0500

    background style update

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(cors());
 const myrateLimit = require("express-rate-limit");
 const thelimit = myrateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 1000,
-  message: "You have exceeded the 1000 requests in 24 hrs limit!",
+  max: 5000,
+  message: "You have exceeded the 5000 requests in 24 hrs limit!",
   headers: true,
 });
 app.use(thelimit);
